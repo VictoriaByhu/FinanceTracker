e1144cd296b33540686ca8d66ba5f3e22d6fa7cf
Byhu Victoria <vikabygu@gmail.com>
2026-05-08 11:21:43 +0300

    Configure Android theme and status bar colors

--- FinanceTracker.MAUI/Platforms/Android/MainActivity.cs
+++ FinanceTracker.MAUI/Platforms/Android/MainActivity.cs
@@ -1,11 +1,34 @@
-﻿using Android.App;
+using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using AndroidX.Core.View;
 
 namespace FinanceTracker.MAUI
 {
-    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    [Activity(Theme = "@style/AppTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnCreate(Bundle? savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            if (Window is null)
+            {
+                return;
+            }
+
+            WindowCompat.SetDecorFitsSystemWindows(Window, true);
+
+            var systemBarsColor = Android.Graphics.Color.ParseColor("#12122A");
+            Window.SetStatusBarColor(systemBarsColor);
+            Window.SetNavigationBarColor(systemBarsColor);
+
+            var controller = WindowCompat.GetInsetsController(Window, Window.DecorView);
+            if (controller is not null)
+            {
+                controller.AppearanceLightStatusBars = false;
+                controller.AppearanceLightNavigationBars = false;
+            }
+        }
     }
 }
